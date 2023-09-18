21aaef13f86346c082cebfb9be891d8bceba4764
Mohammad Maheer <43739258+mohammadmaheer@users.noreply.github.com>
2023-09-18 13:18:56 +0500

    class 16 september

--- Class_4_September_16.dart
+++ Class_4_September_16.dart
@@ -1,3 +1,14 @@
-void main(){
-    
+void main() {
+  for (int i = 2; i <= 10; i++) {
+    for (int j = 1; j <= 10; j++) {
+      print("$i x $j = ${j * i}");
+    }
+  }
+
+  // var map1 = {"Name": "Maheer", "RollNo": "36", "Gender": "Male"};
+  //var map2 = {"Class": "XII", "City": "Karachi", "Country": "Pakistan"};
+  // print(map1);
+  // print(map2);
+
+//  print("${map1}, ${map2}");
 }
