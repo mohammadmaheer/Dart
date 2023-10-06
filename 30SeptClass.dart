71351431460cdfc1f12bd2291dd2902920b6fb4a
Mohammad Maheer <43739258+mohammadmaheer@users.noreply.github.com>
2023-10-07 00:49:28 +0500

    class4

---
+++ 30SeptClass.dart
@@ -0,0 +1,12 @@
+void main() {
+  var emp = Employee();
+  print(emp.name);
+  print(emp.age);
+  print(emp.gneder);
+}
+
+class Employee {
+  String name = "Mohammad Maheer";
+  int age = 30;
+  String gneder = "Male";
+}
